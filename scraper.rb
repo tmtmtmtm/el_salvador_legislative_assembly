71c38da5e03a5d4a0f82fb805b2724d34748f6ea
Jenna Howe <jenna.howe@live.co.uk>
2015-09-13 19:15:00 +0100

    Tidy output, check if entry already in db before scraping

--- scraper.rb
+++ scraper.rb
@@ -18,15 +18,15 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-local = false
-# local = true
+# local = false
+local = true
 sleep_between_requests = 60 # (seconds) be kind to El Salvador's server!
-
-la_url = open('http://asamblea.gob.sv/pleno/pleno-legislativo', read_timeout: 60)
 
 if local
 	require 'pry'
 	la_url = 'http://localhost:8000/pleno_legislativo.html'
+else
+	la_url = open('http://asamblea.gob.sv/pleno/pleno-legislativo', read_timeout: 60)
 end
 
 noko = noko_for(la_url)
@@ -37,41 +37,43 @@ noko.css('dl dt a').each do |a|
 	if local
 		person_url.sub!('asamblea.gob.sv/pleno', 'localhost')
 	end
-	puts person_url
+	puts "url: #{person_url}"
 
 	id = person_url.sub(/.*\//, '')
-	puts id
+	puts "id: #{id}"
 
-	p = noko_for(person_url)
-	name = p.css('h1').text
-	puts name
+	ids = ScraperWiki::select('id from data')
+	if not ids.include?(id)
+		p = noko_for(person_url)
+		name = p.css('h1').text
+		puts "name: #{name}"
 
-	party_class = 'Grupo Parlamentario'
-	group = p.xpath("//span[@class='informacion-diputado'][contains(.,'#{party_class}')]")
-		.first.text.sub(/.*#{party_class}/, '')
-	puts group
+		party_class = 'Grupo Parlamentario'
+		group = p.xpath("//span[@class='informacion-diputado'][contains(.,'#{party_class}')]")
+			.first.text.sub(/.*#{party_class}/, '').tidy
+		puts "faction: #{group}"
 
-	email = p.xpath("//span[.//img[contains(@src,'/emailicon.png')]]/a/@href").text.sub('mailto:', '')
-	puts email
+		email = p.xpath("//span[.//img[contains(@src,'/emailicon.png')]]/a/@href").text.sub('mailto:', '')
+		puts "email: #{email}"
 
-	personal_email = p.xpath("//a[.//img[contains(@src,'personal-emailicon.png')]]/span").text
-	puts personal_email
+		personal_email = p.xpath("//a[.//img[contains(@src,'personal-emailicon.png')]]/span").text
+		puts "personal email: #{personal_email}"
 
-	image = p.xpath("//h1/following-sibling::img[1]/@src").text.sub(/.*\//, "#{person_url}/")
-	puts image
-	
-	data = {
-		id: id,
-		name: name,
-		faction: group.tidy,
-		email: email,
-		email__personal: personal_email,
-		image: image,
-	}
-	ScraperWiki.save_sqlite([:id], data)
-	if local
-		puts data
-		break
+		image = p.xpath("//h1/following-sibling::img[1]/@src").text.sub(/.*\//, "#{person_url}/")
+		puts "image: #{image}\n"
+		
+		data = {
+			id: id,
+			name: name,
+			faction: group,
+			email: email,
+			email__personal: personal_email,
+			image: image,
+		}
+		ScraperWiki.save_sqlite([:id], data)
+
+	else
+		puts "already in database"
 	end
 	sleep(sleep_between_requests)
 end
